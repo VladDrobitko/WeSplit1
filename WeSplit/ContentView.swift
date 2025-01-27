ceed69521900e82590d4f813039dd76cd3d8d69f
VladDrobitko <boss.drobitko@gmail.com>
2025-01-27 16:40:48 +0100

    Finishing the project!(Adding a picker for percentage, and calculation total value)

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,16 +11,58 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tippercentages = [10, 20, 25, 0]
+    
+    var totalPerPeerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = (checkAmount + tipValue) / peopleCount
+        
+        return grandTotal
+        
+    }
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("How much do you tip?")
+                            .font(.system(.subheadline))
+                            .foregroundStyle(Color.secondary)
+                        Picker("Percentage", selection: $tipPercentage) {
+                            ForEach(tippercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                }
+                Section {
+                    Text(totalPerPeerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
-            Section {
-                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
             }
         }
     }
