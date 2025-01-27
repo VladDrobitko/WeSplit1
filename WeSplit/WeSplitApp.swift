c9351cd5be42beaedc4a13ce8a132871ebe19185
VladDrobitko <boss.drobitko@gmail.com>
2025-01-27 14:23:21 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Владислав Дробитько on 25/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
